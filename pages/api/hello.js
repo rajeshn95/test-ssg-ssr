052a8ca33416b9a3aeeb22880e96e59e8c94f612
rajeshn95 <rajesh@betalectic.com>
2022-12-06 11:10:32 +0530

    ssg and ssr

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+// export default async function handler(req, res) {
+//   for (const url of req.body) {
+//     await res.unstable_revalidate(url);
+//   }
+//   res.status(200).json({ revalidate: true });
+// }
--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,9 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
 
+// SSR
+// export async function getServerSideProps({ params }) {
+//   const resp = await fetch(
+//     `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+//   );
+
+//   return {
+//     props: {
+//       pokemon: await resp.json(),
+//     },
+//   };
+// }
+
+// to genrate the all the pages at build time we have to know what all possible ids are there
+// we have to know those ids at build time
 export async function getStaticPaths() {
   const resp = await fetch(
     "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
@@ -14,10 +30,11 @@ export async function getStaticPaths() {
     paths: pokemon.map((pokemon) => ({
       params: { id: pokemon.id.toString() },
     })),
-    fallback: false,
+    fallback: false, // in case id value for which page is not generated, we might need a fallback page
   };
 }
 
+// SSG
 export async function getStaticProps({ params }) {
   const resp = await fetch(
     `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
@@ -27,11 +44,38 @@ export async function getStaticProps({ params }) {
     props: {
       pokemon: await resp.json(),
     },
-    // revalidate: 30,
+    // revalidate: 30, // as static pages generated at build time, and if after build there is some changes in data which we are getting from api in that case we can give option to check the api after 30s when user hits the page
   };
 }
 
 export default function Details({ pokemon }) {
+  // client side rendering
+  // we are making the request
+  // can be cheked in network tab as id.json
+  // check the page source code it should be empty
+  //   const {
+  //     query: { id },
+  //   } = useRouter();
+
+  //   const [pokemon, setPokemon] = useState(null);
+
+  //   useEffect(() => {
+  //     async function getPokemon() {
+  //       const resp = await fetch(
+  //         `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+  //       );
+  //       console.log("helloo");
+  //       setPokemon(await resp.json());
+  //     }
+  //     if (id) {
+  //       getPokemon();
+  //     }
+  //   }, [id]);
+
+  //   if (!pokemon) {
+  //     return null;
+  //   }
+
   return (
     <div>
       <Head>
